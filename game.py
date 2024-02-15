594fb442fb8c8fe8cd0f83542d9105f95d52cc0b
Matheus_Takashi_Maruoka_Vieira <mtmv.snf23@uea.edu.br>
2024-02-15 20:33:56 -0300

    Add Tank

--- game.py
+++ game.py
@@ -30,7 +30,7 @@ class Game:
 
             # tanks
             tanks = []
-            p1 = pygame.Rect(HALF_W, HALF_GH, 48, 48)
+            p1 = Tank(90, 90, 5, 5, "sprites/black_mage.png")
 
             # bullets
             bullets = []
@@ -59,6 +59,25 @@ class Game:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         self.game_start = False
+                    if event.type == pygame.KEYUP:
+                        if event.key == pygame.K_UP:
+                            p1.action = 0
+                        if event.key == pygame.K_UP and event.key == pygame.K_UP:
+                            p1.action = 1
+                        if event.key == pygame.K_RIGHT:
+                            p1.action = 2
+                        if event.key == pygame.K_RIGHT and event.key == pygame.K_DOWN:
+                            p1.action = 3
+                            p1.move_down()
+                            p1.move_right()
+                        if event.key == pygame.K_DOWN:
+                            p1.action = 4
+                        if event.key == pygame.K_DOWN and event.key == pygame.K_LEFT:
+                            p1.action = 5
+                        if event.key == pygame.K_LEFT:
+                            p1.action = 6
+                        if event.key == pygame.K_LEFT and event.key == pygame.K_UP:
+                            p1.action = 7
 
                 screen.fill(RED)
 
@@ -66,6 +85,17 @@ class Game:
                 screen.fill(RED)
                 pygame.draw.rect(screen, YELLOW, p1)  # so pra testar
                 maze.maze_draw(screen, YELLOW)
+
+                # update animation
+                current_time = pygame.time.get_ticks()
+                if current_time - p1.last_update >= p1.animation_cooldown:
+                    p1.frame += 1
+                    p1.last_update = current_time
+                    if p1.frame >= len(p1.animation_list[p1.action]):
+                        p1.frame = 0
+
+                # show frame
+                screen.blit(p1.animation_list[p1.action][p1.frame], (0, 0))
 
                 # update screen
                 pygame.display.flip()
--- tank.py
+++ tank.py
@@ -1,3 +1,42 @@
+import pygame
+import spritesheet
+
+
 class Tank:
-    def __init__(self):
-        pass
+    def __init__(self, sizex, sizey, positionx, positiony, sprite):
+        self.sizex = sizex
+        self.sizey = sizey
+        self.positionx = positionx
+        self.positiony = positiony
+        self.rect = pygame.Rect(self.sizex, self.sizey, self.positionx, self.positiony)
+        self.sprite_sheet_image = pygame.image.load(sprite).convert_alpha()
+        self.sprite_sheet = spritesheet.SpriteSheet(self.sprite_sheet_image)
+        self.animation_list = []
+        self.animation_steps = [1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3]
+        self.action = 0
+        self.last_update = pygame.time.get_ticks()
+        self.animation_cooldown = 100
+        self.frame = 0
+        self.step_counter = 0
+
+        for animation in self.animation_steps:
+            temp_img_list = []
+            for _ in range(animation):
+                temp_img_list.append(self. sprite_sheet.get_image(self.step_counter, 15, 17, 4, (0, 0, 0)))
+                self.step_counter += 1
+            self.animation_list.append(temp_img_list)
+
+    def move_right(self):
+        self.positionx += 5
+
+    def move_left(self):
+        self.positionx -= 5
+
+    def move_up(self):
+
+        self.positiony -= 5
+
+    def move_down(self):
+
+        self.positiony += 5
+
